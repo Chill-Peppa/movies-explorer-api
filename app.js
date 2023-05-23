e46aa68b41d616ba4e055a7b99acb36220cfaa24
Anastasia <www.nastya97@yandex.ru>
2023-05-23 18:07:29 +0300

    feat: connect to db and add models

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// тут будет основное приложение
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
+
+app.listen(PORT, () => {
+  console.log(`Приложение запущено на ${PORT} порту`);
+});
---
+++ models/movie.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Введен не верный адрес.',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Введен не верный адрес.',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Введен не верный адрес.',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRu: {
+    type: String,
+    required: true,
+  },
+  nameEn: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
