1c7fb3cde40c746569bcdda4b60b1c5ebdde8e35
Anastasia <www.nastya97@yandex.ru>
2023-05-25 23:19:06 +0300

    hotfix: fix links in allowed cors

--- utils/constants.js
+++ utils/constants.js
@@ -3,8 +3,8 @@ const urlRegExp = /http(s)?:\/\/(www\.)?[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]+\.[a
 const allowedCors = [
   'https://movies-explorer-diploma.nomoredomains.rocks',
   'http://movies-explorer-diploma.nomoredomains.rocks',
-  'https://api.movies-explorer-diploma.nomoredomains.rocks',
-  'http://api.movies-explorer-diploma.nomoredomains.rocks',
+  'https://api.movies-explorer-dip.nomoredomains.rocks',
+  'http://api.movies-explorer-dip.nomoredomains.rocks',
   'localhost:3000',
   'http://localhost:3000',
   'http://localhost:3001',
